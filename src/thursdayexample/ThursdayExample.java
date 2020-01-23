81f8594069f86f2923f28f9af7a7c0ebdf1d250e
Manvir Sohi <sohimanv@sheridancollege.ca>
2020-01-23 09:52:18 -0500

    Update ThursdayExample.java

--- src/thursdayexample/ThursdayExample.java
+++ src/thursdayexample/ThursdayExample.java
@@ -16,6 +16,7 @@ public class ThursdayExample {
      */
     public static void main(String[] args) {
         System.out.println("Learning GitHub");
+        System.out.println("Learning Pull operations.");
     }
     
 }
